6032082710be0a3d98cd293dd438db9b278044d5
Jxffxjxke <jaffajake25@gmail.com>
2024-08-06 13:59:13 +0100

    longest palindrome odd length

--- JavaScript/longest-palindrome-substring.js
+++ JavaScript/longest-palindrome-substring.js
@@ -1,17 +1,27 @@
 const longestPalindrome = (string) => {
   let palindrome = "";
   let length = string.length;
-  let array = string.split("");
+
+  const expandAroundCenter = (left, right) => {
+    while (left >= 0 && right < length && string[left] === string[right]) {
+      left--;
+      right++;
+    }
+    return string.slice(left + 1, right);
+  };
 
   for (let i = 0; i < length; i++) {
-    let currentPalindrome = array[i];
-    if (array[i - 1] === array[i + 1]) {
-      currentPalindrome = string.slice(i - 1, i + 2);
+    let oddPalindrome = expandAroundCenter(i, i);
+    if (oddPalindrome.length > palindrome.length) {
+      palindrome = oddPalindrome;
     }
-    if (currentPalindrome.length > palindrome.length) {
-      palindrome = currentPalindrome;
+
+    let evenPalindrome = expandAroundCenter(i, i + 1);
+    if (evenPalindrome.length > palindrome.length) {
+      palindrome = evenPalindrome;
     }
   }
+
   return palindrome;
 };
 
