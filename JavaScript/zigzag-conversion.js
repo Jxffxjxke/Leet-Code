05e04b12e0100852ef01ada07e6c59460bac6dc5
Jxffxjxke <jaffajake25@gmail.com>
2024-09-03 11:26:27 +0100

    zigzag conversion

--- JavaScript/zigzag-conversion.js
+++ JavaScript/zigzag-conversion.js
@@ -1,6 +1,23 @@
-const convert = ( string, rows ) =>
-{
-    return Array(rows)
+const convert = function (s, numRows) {
+  if (numRows === 1 || numRows >= s.length) return s;
+
+  const arr = new Array(numRows).fill().map((x) => []);
+
+  let i = 0;
+  let down = true;
+
+  for (const letter of s) {
+    arr[i].push(letter);
+
+    if (i === numRows - 1) down = false;
+    if (i === 0) down = true;
+
+    i += down ? 1 : -1;
+  }
+
+  return arr.reduce((result, current) => {
+    return result.concat(current.join(""));
+  }, "");
 };
 
 const str = "PAYPALISHIRING";
